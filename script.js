61c36aee814fff97147a8711b1f9c4037751c3a1
Kwadrat Projects <wiktorkono@gmail.com>
2022-09-16 22:03:36 +0200

    Small changes preparing for an update

---
+++ script.js
@@ -0,0 +1,16 @@
+// const themebtn = document.querySelector("#change-theme");
+// const main = document.querySelector("main");
+// let mode = 1;
+
+// themebtn.addEventListener('click', function(){
+//     if(mode == 1) {
+//         main.style.backgroundColor = "black";
+//         console.log("Black");
+//         mode = 0;
+//     }
+//     else {
+//         main.style.backgroundColor = "whitesmoke";
+//         console.log("Włodzimierz");
+//         mode = 1;
+//     }
+// });
