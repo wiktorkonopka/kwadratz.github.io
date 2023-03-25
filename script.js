cbcd6d1ed13c353973b492020ee7fdfea263afb8
Kwadrat Projects <wiktorkono@gmail.com>
2023-03-25 16:29:08 +0100

    "small" update - site overhaul with new blog post

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// const themebtn = document.querySelector("#change-theme");
-// const main = document.querySelector("main");
-// let mode = 1;
-
-// themebtn.addEventListener('click', function(){
-//     if(mode == 1) {
-//         main.style.backgroundColor = "black";
-//         console.log("Black");
-//         mode = 0;
-//     }
-//     else {
-//         main.style.backgroundColor = "whitesmoke";
-//         console.log("Włodzimierz");
-//         mode = 1;
-//     }
-// });
+function copyText(text) {
+   navigator.clipboard.writeText(text);
+   alert('Copied!');
+} 
