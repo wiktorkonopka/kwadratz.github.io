8b030b4cdc1fa60fc44b083a497506e54d4efc50
Kwadrat Projects <wiktorkono@gmail.com>
2023-06-06 17:26:39 +0200

    critical isUnderConstruction thing added

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function copyText(text) {
    navigator.clipboard.writeText(text);
    alert('Copied!');
-} 
+};
+
+let isUnderConstruction = true;
+
+if (isUnderConstruction == true) {
+   alert("The page is under construction right now, come back later!")
+   window.location.href = "about:blank"
+}
